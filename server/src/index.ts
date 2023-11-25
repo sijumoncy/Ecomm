64d77e61c8c34f1d6c85e6127e4ed93ec3e48718
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2023-11-25 15:04:07 +0530

    setup log with morgan and add json parser

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import morgan from 'morgan'
 
 dotenv.config()
 const app = express()
 
 const apiBaseUrl = process.env.API_URL
 
+// middlewares
+app.use(bodyParser.json())
+app.use(morgan("tiny"))
+
 // default route
-app.get(apiBaseUrl+'/', (req, res) => {
+app.get(`${apiBaseUrl}/`, (req, res) => {
     res.send('App is Up and Running')
 })
 
